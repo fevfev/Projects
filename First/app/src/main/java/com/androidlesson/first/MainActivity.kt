b4f57cb47bc9b7be9ef7b3d98a77665a44a687db
Android Lessons <fev.fev@ya.ru>
2024-12-18 00:22:37 +0300

    Add First App Example
    Add First App Example

---
+++ First/app/src/main/java/com/androidlesson/first/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.androidlesson.first
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.androidlesson.first.ui.theme.FirstTheme
+
+// Главная активность приложения
+class MainActivity : ComponentActivity() {
+    // Метод, вызываемый при создании активности
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Включение режима Edge-to-Edge
+        enableEdgeToEdge()
+        // Установка содержимого активности
+        setContent {
+            // Применение темы
+            FirstTheme {
+                // Использование Scaffold для создания базовой структуры экрана
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Вызов функции Greeting с передачей имени и отступов
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+// Компонент Compose, отображающий приветственное сообщение
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    // Текстовый элемент, отображающий приветствие
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+// Предпросмотр компонента Greeting в Android Studio
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    // Применение темы
+    FirstTheme {
+        // Вызов функции Greeting с передачей имени
+        Greeting("Android")
+    }
+}
